9a0958d732066347b1cc5aeddc0cba9d7c1e85c5
Cesar-caza-epng <159482256+Cesar-caza-epng@users.noreply.github.com>
2024-05-15 21:35:31 -0600

    waewewa

---
+++ piton/Fighting.py
@@ -0,0 +1,26 @@
+import pygame as py
+import sys
+from Chrcter import p1
+from floor import suelito
+
+def iniciarpant():
+    py.init()
+    personaj=p1()
+    screen=py.display.set_mode((1000,700))
+    piso=suelito()
+    sprites=py.sprite.Group()
+    sprites.add(piso)
+    sprites.add(personaj)
+    while True:
+        for event in py.event.get():
+            if event.type==py.QUIT:
+                py.quit()
+                sys.exit()
+        screen.fill((255,255,255))
+        keys=py.key.get_pressed()
+        sprites.draw(screen)
+        sprites.update(keys,piso.rect)
+        py.display.flip()
+
+
+iniciarpant()
