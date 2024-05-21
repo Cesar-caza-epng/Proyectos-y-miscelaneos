04abf633df3dc8d3dfcf9a85eb5c321f186ef8a2
Cesar-caza-epng <159482256+Cesar-caza-epng@users.noreply.github.com>
2024-05-20 22:17:14 -0600

    wewaew

--- Jueguito_JS/mobile.js
+++ Jueguito_JS/mobile.js
@@ -1,17 +1,28 @@
 perso=document.getElementById("nia");
 canv=document.getElementById("canvas");
+bot=document.getElementById("botonjuego");
+ene=document.getElementById("ene");
+let cooldown=false
+let juegoini=false;
+bot.addEventListener("click", function(){
+    if(juegoini===false){
+        Empzr_Juego();
+    }
+});
 let pressD=false
 let posLpersonaje=parseInt(getComputedStyle(perso).Left,10)
 let posTpersonaje=parseInt(getComputedStyle(perso).top,10)
 let pressI=false
 let pressA=false
 let pressAB=false
-let canvaR=canv.offsetLeft+canv.offsetWidth;
-let canvaL=canv.offsetLeft;
-let canvaT=canv.offsetTop;
-let canvaB=canv.offsetTop+canv.offsetHeight;
+
 
 function Empzr_Juego(){
+    let canvaR=canv.offsetLeft+canv.offsetWidth;
+    let canvaL=canv.offsetLeft;
+    let canvaT=canv.offsetTop;
+    let canvaB=canv.offsetTop+canv.offsetHeight;
+    juegoini=true;
     perso.style.left=posLpersonaje;
     perso.style.top=posTpersonaje;
     document.addEventListener("keydown" ,function(event){
@@ -29,6 +40,10 @@ function Empzr_Juego(){
 
         if(event.key==="s" || event.key==="S"){
             pressAB=true;
+        }
+
+        if((event.key==="u" || event.key==="U") && cooldown===false){
+            atacac();
         }
     });
 
@@ -49,6 +64,32 @@ function Empzr_Juego(){
             pressAB=false;
         }
     });
+
+    function atacac(){
+        cooldown=true;
+        if((parseInt(getComputedStyle(ene).left,10)>parseInt(getComputedStyle(perso).left,10) && parseInt(getComputedStyle(ene).left,10)<parseInt(getComputedStyle(perso).left,10)+200) && (parseInt(getComputedStyle(ene).top,10)+130>=parseInt(getComputedStyle(perso).top,10) && parseInt(getComputedStyle(ene).top,10)-130<parseInt(getComputedStyle(perso).top,10))){
+            console.log("TOCOOOOOO");
+        }
+
+        ataque=document.createElement("img");
+        ataque.style.position="fixed";
+        ataque.src="Imagenes/eplochion.webp"
+        ataque.style.width=140 + "px";
+        ataque.style.height=140 + "px";
+        ataque.style.left=parseInt(getComputedStyle(perso).left,10)+100-40 +"px";
+        ataque.style.top=parseInt(getComputedStyle(perso).top,10)+130-40 +"px";
+        canv.appendChild(ataque);
+        console.log(ataque);
+        setTimeout(acabarcac, 500);
+    }
+
+    
+    function acabarcac(){
+        if(canv.contains(ataque)){
+            canv.removeChild(ataque);       
+        }
+        cooldown=false
+    }
 
     function mover(){
         posleft = parseInt(getComputedStyle(perso).left,10);
@@ -78,5 +119,3 @@ function Empzr_Juego(){
 
     mover()
 }
-
-Empzr_Juego();
